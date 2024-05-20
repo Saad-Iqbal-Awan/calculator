a4ad7949539fc3f8c74157bab6d275b5ca1d7c0a
M. Saad Iqbal <114656892+Saad-Iqbal-Awan@users.noreply.github.com>
2024-05-20 15:36:07 +0200

    add arithematic functions and stylesheet

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
-console.log('works');
+console.log('works'); 
+
+
+let numOne = 20;
+let numTwo = 15;
+let operand = "subtract"
+let result;
+
+function operate (numOne, numTwo, operand) {
+    if (operand === 'add') {
+        result = add(numOne, numTwo);
+    }
+
+    else if (operand === 'subtract') {
+        result = subtract(numOne, numTwo);
+    }
+
+    else if (operand === 'multiply') {
+        result = multiply(numOne, numTwo);
+    }
+
+    else if (operand === 'divide') {
+        result = divide(numOne, numTwo);
+    }
+
+    else {
+        console.log('invalid');
+    }
+    
+}
+operate(numOne, numTwo, operand);
+console.log(result);
+
+function add(a, b) { 
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) { 
+    return a * b;
+}
+
+function divide(a, b) { 
+    return a / b;
+}
+
+// console.log(add(numOne, numTwo))
+// console.log(multiply(20, 15))
+// console.log(subtract(20, 15))
+// console.log(divide(20, 15)) 
